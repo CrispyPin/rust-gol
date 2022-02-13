5ccb6ced6d817a5bd84be56bce81e20c1b46155e
CrispyPin <crispin@tasa.se>
2022-02-13 21:48:31 +0100

    cleanup & optimise, add timing prints to debug builds

--- src/main.rs
+++ src/main.rs
@@ -3,55 +3,67 @@ use std::time;
 use std::thread;
 
 const SIZE: usize = 48;
+const ALIVE: &str = "[]";
+const DEAD: &str = "..";
 
-type Board = ( [[bool; SIZE]; SIZE] );
-type BoardCount = ( [[u8; SIZE]; SIZE] );
+type Board<T> = [[T; SIZE]; SIZE];
 
 
 fn main() {
-	let mut board: Board = [[false; SIZE]; SIZE];
-	//draw(board);
+	let mut board: Board<bool> = [[false; SIZE]; SIZE];
 	populate(&mut board);
+	print!("\x1B[2J"); // clear screen
+
 	loop {
-		println!("\n\n\n\n\n\n\n\n\n\n\n\n");
-		draw(board);
-		step(&mut board);
+		print!("\x1B[u"); // reset cursor
+
+		#[cfg(debug_assertions)]
+		{
+			let draw_start = time::Instant::now();
+			draw(board);
+			println!("\n");
+			println!("Time to draw: {:.0?}", draw_start.elapsed());
+			
+			let step_start = time::Instant::now();
+			step(&mut board);
+			println!("Time to step: {:.0?}", step_start.elapsed());
+		}
+		#[cfg(not(debug_assertions))]
+		{
+			draw(board);
+			step(&mut board);
+		}
+
 		thread::sleep(time::Duration::from_millis(100));
 	}
 }
 
-fn step(board: &mut Board) {
-	let mut counts: BoardCount = [[0; SIZE]; SIZE];
+fn step(board: &mut Board<bool>) {
+	let mut counts: Board<u8> = [[0; SIZE]; SIZE];
+	// count neighbor cells
 	for row in 0..SIZE {
 		for col in 0..SIZE {
 			let mut count: u8 = 0;
 			for y in 0..3 {
 				for x in 0..3 {
-					if board
-					[(row + y + SIZE - 1) % SIZE]
-					[(col + x + SIZE - 1) % SIZE] {
-						count += 1;
-					}
+					count += board
+							[(row + y + SIZE - 1) % SIZE]
+							[(col + x + SIZE - 1) % SIZE] as u8;
 				}
 			}
 			counts[row][col] = count;
 		}
 	}
+	// apply rule
 	for y in 0..SIZE {
 		for x in 0..SIZE {
 			let count = counts[y][x];
-			
-			if board[y][x] {
-				board[y][x] = count == 3 || count == 4;
-			} else {
-				board[y][x] = count == 3;
-			}
+			board[y][x] = count == 3 || (count == 4 && board[y][x]);
 		}
 	}
 }
 
-
-fn populate(board: &mut Board) {
+fn populate(board: &mut Board<bool>) {
 	for row in board.iter_mut() {
 		for cell in row.iter_mut() {
 			*cell = rand::random();
@@ -59,15 +71,17 @@ fn populate(board: &mut Board) {
 	}
 }
 
-fn draw(board: Board) {
+fn draw(board: Board<bool>) {
+	let mut r: String;
 	for row in board.iter() {
+		r = String::new();
 		for cell in row.iter() {
 			if *cell {
-				print!("[]");
+				r += ALIVE;
 			} else {
-				print!("  ");
+				r += DEAD;
 			}
 		}
-		println!();
+		println!("{}", r);
 	}
 }
